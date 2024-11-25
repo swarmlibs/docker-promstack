e5cf7609ad52aea1a8c4f5aa41a20d2e54cc85fe
Socheat Sok <socheatsok78@gmail.com>
2024-11-25 23:40:44 +0700

    Attach the externally created network to stack namespace
    This will allow the docker stack rm to also remove the networks if not in-used

--- rootfs/promstack.sh
+++ rootfs/promstack.sh
@@ -52,21 +52,36 @@ if [[ "${1}" == "install" ]]; then
 	}
 
 	cd "${PROMSTACK_TMPDIR}" && {
-		entrypoint_log "$ME: Checking for required networks..."
+		entrypoint_log "$ME: Deploying promstack stack..."
+
 		if ! docker network inspect public >/dev/null 2>&1; then
-			entrypoint_log "$ME: Creating 'public' network..."
-			docker network create --scope=swarm --driver=overlay --attachable public >/dev/null
+			entrypoint_log "$ME: - Creating network public"
+			docker network create \
+					--driver=overlay \
+					--scope=swarm \
+					--attachable \
+					--label=com.docker.stack.namespace=promstack \
+				public >/dev/null
 		fi
 		if ! docker network inspect prometheus >/dev/null 2>&1; then
-			entrypoint_log "$ME: Creating 'prometheus' network..."
-			docker network create --scope=swarm --driver=overlay --attachable prometheus >/dev/null
+			entrypoint_log "$ME: - Creating network prometheus"
+			docker network create \
+					--driver=overlay \
+					--scope=swarm \
+					--attachable \
+					--label=com.docker.stack.namespace=promstack \
+				prometheus >/dev/null
 		fi
 		if ! docker network inspect prometheus_gwnetwork >/dev/null 2>&1; then
-			entrypoint_log "$ME: Creating 'prometheus_gwnetwork' network..."
-			docker network create --scope=swarm --driver=overlay --attachable prometheus_gwnetwork >/dev/null
+			entrypoint_log "$ME: - Creating network prometheus_gwnetwork"
+			docker network create \
+					--driver=overlay \
+					--scope=swarm \
+					--attachable \
+					--label=com.docker.stack.namespace=promstack \
+				prometheus_gwnetwork >/dev/null
 		fi
 
-		entrypoint_log "$ME: Deploying promstack stack..."
 		docker stack deploy \
 				${DOCKER_STACK_DEPLOY_ARGS} \
 				--quiet \
@@ -117,26 +132,9 @@ elif [[ "${1}" == "upgrade" ]]; then
 elif [[ "${1}" == "uninstall" ]]; then
 	entrypoint_log "$ME: Attempting to remove the promstack..."
 	if docker stack ls --format "{{.Name}}" | grep promstack >/dev/null; then
-		docker stack rm promstack && {
-			echo "$ME: Grace period to allow services to shutdown..."
-			sleep 15
-		}
-	fi
-
-	entrypoint_log "$ME: Attempting to remove networks..."
-	if docker network inspect prometheus >/dev/null 2>&1; then
-		if docker network rm prometheus >/dev/null 2>&1; then
-			entrypoint_log "$ME: The 'prometheus' network removed..."
-		else
-			entrypoint_log "$ME: The 'prometheus' network is not removable. It may be in use by other services."
-		fi
-	fi
-	if docker network inspect prometheus_gwnetwork >/dev/null 2>&1; then
-		if docker network rm prometheus_gwnetwork >/dev/null 2>&1; then
-			entrypoint_log "$ME: The 'prometheus_gwnetwork' network removed..."
-		else
-			entrypoint_log "$ME: The 'prometheus_gwnetwork' network is not removable. It may be in use by other services."
-		fi
+		docker stack rm promstack | while read line; do entrypoint_log "$ME: - $line"; done
+		echo "$ME: Grace period to allow services to shutdown..."
+		sleep 15
 	fi
 
 	entrypoint_log "$ME:"
